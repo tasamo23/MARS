9a6012a9ebfd57a7caaa3a4b25f022bb3be31a7d
Masato Arnold <dev@masatoarnold.dev>
2025-02-01 08:58:27 +0900

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+# MARS
+**M**asato **A**rnold's **R**amble **S**ite
+
+## Acknowledgement
+
+Theme is based on [Astro-sphere](https://github.com/markhorn-dev/astro-sphere). Check it out!
---
+++ src/consts.ts
@@ -0,0 +1,75 @@
+import type { Site, Page, Links, Socials } from "@types"
+
+// Global
+export const SITE: Site = {
+  TITLE: "MARS",
+  DESCRIPTION: "Welcome to MARS, my personal portfolio and blog.",
+  AUTHOR: "Masato Arnold",
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: "Work",
+  DESCRIPTION: "Places I have worked.",
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: "Blog",
+  DESCRIPTION: "Writing on topics I am passionate about.",
+}
+
+// Projects Page 
+export const PROJECTS: Page = {
+  TITLE: "Projects",
+  DESCRIPTION: "Recent projects I have worked on.",
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: "Search",
+  DESCRIPTION: "Search all posts and projects by keyword.",
+}
+
+// Links
+export const LINKS: Links = [
+  {
+    TEXT: "Home",
+    HREF: "/",
+  },
+  {
+    TEXT: "Work",
+    HREF: "/work",
+  },
+  {
+    TEXT: "Blog",
+    HREF: "/blog",
+  },
+  {
+    TEXT: "Projects",
+    HREF: "/projects",
+  },
+]
+
+// Socials
+export const SOCIALS: Socials = [
+  {
+    NAME: "Email",
+    ICON: "email",
+    TEXT: "dev@masatoarnold.dev",
+    HREF: "mailto:dev@masatoarnold.dev",
+  },
+  {
+    NAME: "Github",
+    ICON: "github",
+    TEXT: "tasamo23",
+    HREF: "https://github.com/tasamo23"
+  },
+  {
+    NAME: "LinkedIn",
+    ICON: "linkedinn",
+    TEXT: "masatoarnold",
+    HREF: "https://www.linkedin.com/in/masatoarnold/",
+  }
+]
+
